918d65499ea2ead174df70ba52a104924ffc07fa
Bissa <divyanshubissa99@gmail.com>
2023-06-26 15:40:33 +0530

    added file

---
+++ src/screens/Cart.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import Delete from '@material-ui/icons/Delete'
+import { useCart, useDispatchCart } from '../components/ContextReducer';
+export default function Cart() {
+  let data = useCart();
+  let dispatch = useDispatchCart();
+  if (data.length === 0) {
+    return (
+      <div>
+        <div className='m-5 w-100 text-center fs-3'>The Cart is Empty!</div>
+      </div>
+    )
+  }
+  // const handleRemove = (index)=>{
+  //   console.log(index)
+  //   dispatch({type:"REMOVE",index:index})
+  // }
+
+  const handleCheckOut = async () => {
+    let userEmail = localStorage.getItem("userEmail");
+    // console.log(data,localStorage.getItem("userEmail"),new Date())
+    let response = await fetch("http://localhost:5000/api/auth/orderData", {
+      // credentials: 'include',
+      // Origin:"http://localhost:3000/login",
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        order_data: data,
+        email: userEmail,
+        order_date: new Date().toDateString()
+      })
+    });
+    console.log("JSON RESPONSE:::::", response.status)
+    if (response.status === 200) {
+      dispatch({ type: "DROP" })
+    }
+  }
+
+}
