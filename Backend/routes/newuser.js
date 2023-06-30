67320c9c7a34f960fdcbff33e706a345e8922dcf
Harsh Tiwary <itsharshofficial27@gmail.com>
2023-06-30 21:56:09 +0530

    saving new user

---
+++ Backend/routes/newuser.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const User = require('../models/User')
+const Order = require('../models/Orders')
+const router = express.Router()
+const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs')
+var jwt = require('jsonwebtoken');
+
+const fetch = require('../middleware/fetchdetails');
+const jwtSecret = "HaHa"
+// var foodItems= require('../index').foodData;
+// require("../index")
+//Creating a user and storing data to MongoDB Atlas, No Login Requiered
+router.post('/createuser', [
+    body('email').isEmail(),
+    body('password').isLength({ min: 5 }),
+    body('name').isLength({ min: 3 })
+], async (req, res) => {
+    let success = false
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ success, errors: errors.array() })
+    }
