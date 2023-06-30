2ba80ea27e040c2cfae8e2d15d7e4b971aed770d
Snehal Tripathi <snehalprdc2003@gmail.com>
2023-06-30 18:07:09 +0530

    changes in Home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -62,34 +62,34 @@ export default function Home() {
                     </button>
                 </div>
             </div>
-            <div className='container'> {/* boootstrap is mobile first */}
-                {
-                    foodCat !== []
-                        ? foodCat.map((data) => {
-                            return (
-                                // justify-content-center
-                                <div className='row mb-3'>
-                                    <div key={data.id} className='fs-3 m-3'>
-                                        {data.CategoryName}
-                                    </div>
-                                    <hr id="hr-success" style={{ height: "4px", backgroundImage: "-webkit-linear-gradient(left,rgb(0, 255, 137),rgb(0, 0, 0))" }} />
-                                    {foodItems !== [] ? foodItems.filter(
-                                        (items) => (items.CategoryName === data.CategoryName) && (items.name.toLowerCase().includes(search.toLowerCase())))
-                                        .map(filterItems => {
-                                            return (
-                                                <div key={filterItems.id} className='col-12 col-md-6 col-lg-3'>
-                                                    {console.log(filterItems.url)}
-                                                    <Card foodName={filterItems.name} item={filterItems} options={filterItems.options[0]} ImgSrc={filterItems.img} ></Card>
-                                                </div>
-                                            )
-                                        }) : <div> No Such Data </div>}
+            {/* <div className='container'> {/* boootstrap is mobile first */}
+            {
+                foodCat !== []
+                    ? foodCat.map((data) => {
+                        return (
+                            // justify-content-center
+                            <div className='row mb-3'>
+                                <div key={data.id} className='fs-3 m-3'>
+                                    {data.CategoryName}
                                 </div>
-                            )
-                        })
-                        : ""}
-            </div>
-            <Footer />
-        </div>
+                                <hr id="hr-success" style={{ height: "4px", backgroundImage: "-webkit-linear-gradient(left,rgb(0, 255, 137),rgb(0, 0, 0))" }} />
+                                {foodItems !== [] ? foodItems.filter(
+                                    (items) => (items.CategoryName === data.CategoryName) && (items.name.toLowerCase().includes(search.toLowerCase())))
+                                    .map(filterItems => {
+                                        return (
+                                            <div key={filterItems.id} className='col-12 col-md-6 col-lg-3'>
+                                                {console.log(filterItems.url)}
+                                                <Card foodName={filterItems.name} item={filterItems} options={filterItems.options[0]} ImgSrc={filterItems.img} ></Card>
+                                            </div>
+                                        )
+                                    }) : <div> No Such Data </div>}
+                            </div>
+                        )
+                    })
+                    : ""}
+        </div> * /}
+        < Footer />
+        </div >
 
 
 
