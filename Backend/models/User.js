fb30ab3fbf27b4b936513639deba0d5c6da7016a
Bissa <divyanshubissa99@gmail.com>
2023-07-10 19:48:38 +0530

    updated

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,30 +1,22 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const { Schema } = mongoose;
-
-const UserSchema = new Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    location:{
-        type:String,
-        required:true,
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true
-    },
-    password:{
-        type:String,
-        required:true
-    },
-    date:{
-        type:Date,
-        default:Date.now
-    },
-
-  });
-
-  module.exports = mongoose.model('user',UserSchema)
+const user = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+const nuser = mongoose.model("newusers", user);
+module.exports = nuser;
