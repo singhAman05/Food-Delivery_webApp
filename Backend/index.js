ed6ac31477f3d6b4b73ba016cc5fb87fd8be4a1d
singhAman05 <amanshankarsingh05@gmail.com>
2024-06-22 23:46:42 +0530

    new site domain in cors

--- Backend/index.js
+++ Backend/index.js
@@ -10,14 +10,12 @@ const errorHandler = require("./middleware/errorMiddleware");
 //to access react api in our system of backend
 app.use(cors());
 app.use((req, res, next) => {
-  res.setHeader(
+  res.header(
     "Access-Control-Allow-Origin",
     "https://crave-express-nine.vercel.app"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  ); // Replace with your frontend domain
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 
