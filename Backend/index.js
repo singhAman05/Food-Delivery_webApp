2f0aa8e1d14ff79f7d45c01cd5f0e4e6e752c1eb
singhAman05 <amanshankarsingh05@gmail.com>
2024-06-22 23:43:03 +0530

    new site domain in cors

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://crave-express-woad.vercel.app"
+    "https://crave-express-nine.vercel.app"
   );
   res.header(
     "Access-Control-Allow-Headers",
